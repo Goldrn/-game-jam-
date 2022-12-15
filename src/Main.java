1f2a0e43ea7aea24c25bb8895b1e7d464f89dd82
Goldrn <goldrn@users.noreply.github.com>
2022-12-15 14:30:26 -0500

    eek

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static boolean turns = true;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         player.x_pos = 5;
         player.y_pos = 5;
+        File wurld = new File("/home/goldrn/programing/java/all-nighter-game-jam/src/test.txt");
         while(turns) {
             entities.create_entitys();
-            world.create_world('.', world.world_width, world.world_height);
+            world.create_world('.', world.world_width, world.world_height, wurld);
             input_handler.input();
             input_handler.piss_poor_ifs();
         }
--- src/world.java
+++ src/world.java
@@ -1,13 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class world {
-    public static int world_width = 11;
-    public static int world_height = 11;
+    public static int world_width = 29;
+    public static int world_height = 29;
 
-    public static void create_world(char ground, int width, int height) {
+    public static void create_world(char ground, int width, int height, File lvl) throws FileNotFoundException {
         char[][] world = new char[width][height];
+        Scanner scanner = new Scanner(lvl);
 
-        for(int i = 0; i < height; i++) {
+        for(int i = 0; scanner.hasNextLine() && i < height; i++) {
+            char[] chars = scanner.nextLine().toCharArray();
             for (int j = 0; j < width; j++) {
-                world[i][j] = ground;
+                world[i][j] = chars[j];
                 player.draw_char(world);
                 entities.rock.create_rock(world, 3, 7);
                 System.out.print(world[i][j] + " ");
